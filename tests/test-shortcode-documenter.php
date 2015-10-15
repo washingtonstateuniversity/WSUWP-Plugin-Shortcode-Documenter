6252fa6eacf6b015c17494c4bdc9867bc8363e66
jeremyfelt <jeremy.felt@gmail.com>
2015-10-15 12:11:46 -0700

    Build out additional test cases

--- tests/test-shortcode-documenter.php
+++ tests/test-shortcode-documenter.php
@@ -18,7 +18,10 @@ class Test_Shortcode_Documenter extends WP_UnitTestCase {
 		$data = array(
 			array( '[shortcode_doc]', '' ),
 			array( '[shortcode_doc shortcode=""]', '' ),
-			array( '[shortcode_doc shortcode="wsuwp_json"]', '<pre><code>[wsuwp_json]</code></pre>'),
+			array(
+				'[shortcode_doc shortcode="wsuwp_json"]',
+				'<pre><code>[wsuwp_json]</code></pre>'
+			),
 			array(
 				'[shortcode_doc shortcode="wsuwp_json"]Text content[/shortcode_doc]',
 				'<code>[wsuwp_json]Text content[/wsuwp_json]</code>',
@@ -38,6 +41,10 @@ class Test_Shortcode_Documenter extends WP_UnitTestCase {
 			array(
 				'[shortcode_doc shortcode="wsuwp_json" atts="count,output,empty" values="4,headlines,"]',
 				'<pre><code>[wsuwp_json count="4" output="headlines" empty=""]</code></pre>',
+			),
+			array(
+				'[shortcode_doc shortcode="wsuwp_json" atts="count,output,empty" values="4,headlines,"]<div>HTML content</div>[/shortcode_doc]',
+				'<code>[wsuwp_json count="4" output="headlines" empty=""]&lt;div&gt;HTML content&lt;/div&gt;[/wsuwp_json]</code>',
 			)
 		);
 
