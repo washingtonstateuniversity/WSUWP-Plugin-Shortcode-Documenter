e3c76f9a2ad9ff29a84c6e46d6658e2c67f16df7
jeremyfelt <jeremy.felt@gmail.com>
2015-10-15 11:09:56 -0700

    Adjust tests to use a data provider for basic responses

--- tests/test-shortcode-documenter.php
+++ tests/test-shortcode-documenter.php
@@ -2,10 +2,23 @@
 
 class MyTestClass extends WP_UnitTestCase {
 
-	public function test_shortcode_documenter_default_response() {
-		$content = '[shortcode_doc]';
+	/**
+	 * @dataProvider data_shortcode_response
+	 */
+	public function test_shortcode_documenter_response( $content, $response ) {
 		$content = do_shortcode( $content );
 
-		$this->assertEquals( '', $content );
+		$this->assertEquals( $response, $content );
+	}
+
+	/**
+	 * List of content blocks and expected responses.
+	 */
+	public function data_shortcode_response() {
+		$data = array(
+			array( '[shortcode_doc]', '' ),
+		);
+
+		return $data;
 	}
 }
